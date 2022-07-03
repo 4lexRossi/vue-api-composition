36859a33d8aa2e2eae70b19ed661b8e3d47f9fac
4lexRossi <devalexrossi@gmail.com>
2022-07-03 18:54:37 -0300

    add state manager with pinia

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -10,6 +10,14 @@
   </ul>
 
   <textarea v-autofocus />
+
+  <div>
+    <button
+    @click="counter.increaseCounter(1)"
+      class="counter-button"
+      :class="{ yellow: counter.oddOrEven === 'odd' }"
+    >{{counter.count}}</button>
+  </div>
 </template>
 
 <script setup>
@@ -18,6 +26,7 @@
 */
 import { vAutofocus } from '@/directives/vAutofocus';
 import { ref } from 'vue';
+import { useCounterStore } from '@/stores/counter';
 
 /*
   posts
@@ -36,10 +45,25 @@ const posts = ref([
     title: 'Post 3',
   },
 ]);
+
+/*
+  counter
+*/
+const counter = useCounterStore();
+
 </script>
 
 <style scoped>
 ul {
   margin-bottom: 1em;
+}
+.counter-button {
+  width: 100%;
+  background-color: grey;
+  border-radius: 0.3em;
+  font-size: 3em;
+}
+.yellow {
+  background-color: yellow;
 }
 </style>
